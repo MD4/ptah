56a935814f14224f2556bdd6025b00ada1f2ae59
MD4 <m.dequa@gmail.com>
2024-11-02 02:47:36 +0100

    fix(*): reverted `tsup` to 7.x.x - watch was broken

--- apps/service-gateway-ws/src/index.ts
+++ apps/service-gateway-ws/src/index.ts
@@ -1,54 +1,54 @@
-import "dotenv/config";
-import { log, logError } from "@ptah/lib-logger";
-import type { PubsubChannel, PubsubMessage } from "@ptah/lib-models";
-import { services } from "@ptah/lib-shared";
+import "dotenv/config"
+import { log, logError } from "@ptah/lib-logger"
+import type { PubsubChannel, PubsubMessage } from "@ptah/lib-models"
+import { services } from "@ptah/lib-shared"
 
-import * as server from "./server";
+import * as server from "./server"
 
 const kill = async (gracefully: boolean): Promise<void> => {
-  log(process.env.SERVICE_NAME, "killing...");
-  services.pubsub.disconnect();
+  log(process.env.SERVICE_NAME, "killing...")
+  services.pubsub.disconnect()
   if (gracefully) {
-    await server.stop();
+    await server.stop()
   } else {
-    void server.stop();
+    void server.stop()
   }
-  log(process.env.SERVICE_NAME, "killed.");
-  process.exitCode = gracefully ? 0 : 1;
-  process.exit();
-};
+  log(process.env.SERVICE_NAME, "killed.")
+  process.exitCode = gracefully ? 0 : 1
+  process.exit()
+}
 
-const killVoid = (gracefully: boolean) => (): void => void kill(gracefully);
+const killVoid = (gracefully: boolean) => (): void => void kill(gracefully)
 
 const main = async (): Promise<void> => {
-  log(process.env.SERVICE_NAME, "starting..");
+  log(process.env.SERVICE_NAME, "starting..")
 
-  process.on("SIGINT", killVoid(true));
-  process.on("SIGTERM", killVoid(true));
-  process.on("SIGKILL", killVoid(false));
+  process.on("SIGINT", killVoid(true))
+  process.on("SIGTERM", killVoid(true))
+  // process.on("SIGKILL", killVoid(false));
 
-  const channels: PubsubChannel[] = ["midi", "system"];
+  const channels: PubsubChannel[] = ["midi", "system"]
 
   await Promise.all([
     services.pubsub.connect(
       channels,
       (channel: PubsubChannel, message: PubsubMessage) => {
         if (message.type === "clock:tick") {
-          return; // ignore clock:tick to avoid ws flooding
+          return // ignore clock:tick to avoid ws flooding
         }
 
-        server.broadcast(channel, message);
-      },
+        server.broadcast(channel, message)
+      }
     ),
     server.start(channels, (channel: PubsubChannel, message: PubsubMessage) => {
-      services.pubsub.send(channel, message);
+      services.pubsub.send(channel, message)
     }),
-  ]);
+  ])
 
-  log(process.env.SERVICE_NAME, "service is running");
-};
+  log(process.env.SERVICE_NAME, "service is running")
+}
 
 main().catch((error: unknown) => {
-  logError(process.env.SERVICE_NAME, error);
-  return kill(false);
-});
+  logError(process.env.SERVICE_NAME, error)
+  return kill(false)
+})
--- apps/service-main/src/index.ts
+++ apps/service-main/src/index.ts
@@ -1,41 +1,40 @@
-import "dotenv/config";
-import { log, logError } from "@ptah/lib-logger";
-import { services } from "@ptah/lib-shared";
+import "dotenv/config"
+import { log, logError } from "@ptah/lib-logger"
+import { services } from "@ptah/lib-shared"
 
-import { handleMessage } from "./handlers/message-handlers";
-import * as dmx from "./utils/dmx";
+import { handleMessage } from "./handlers/message-handlers"
+import * as dmx from "./utils/dmx"
 
 const kill = (gracefully: boolean): void => {
-  log(process.env.SERVICE_NAME, "killing...");
-  services.pubsub.disconnect();
-  log(process.env.SERVICE_NAME, "killed.");
-  process.exitCode = gracefully ? 0 : 1;
-  process.exit();
-};
+  log(process.env.SERVICE_NAME, "killing...")
+  services.pubsub.disconnect()
+  log(process.env.SERVICE_NAME, "killed.")
+  process.exitCode = gracefully ? 0 : 1
+  process.exit()
+}
 
 const killVoid = (gracefully: boolean) => (): void => {
-  kill(gracefully);
-};
+  kill(gracefully)
+}
 
 const main = async (): Promise<void> => {
-  log(process.env.SERVICE_NAME, "starting..");
-
-  process.on("SIGINT", killVoid(true));
-  process.on("SIGTERM", killVoid(true));
-  process.on("SIGKILL", killVoid(false));
+  log(process.env.SERVICE_NAME, "starting..")
+  process.on("SIGINT", killVoid(true))
+  process.on("SIGTERM", killVoid(true))
+  // process.on("SIGKILL", killVoid(false));
 
   await Promise.all([
     services.pubsub.connect(
       ["midi", "system"],
-      (channel, message) => void handleMessage(channel, message),
+      (channel, message) => void handleMessage(channel, message)
     ),
     dmx.initialize(),
-  ]);
+  ])
 
-  log(process.env.SERVICE_NAME, "service is running");
-};
+  log(process.env.SERVICE_NAME, "service is running")
+}
 
 main().catch((error: unknown) => {
-  logError(process.env.SERVICE_NAME, error);
-  kill(false);
-});
+  logError(process.env.SERVICE_NAME, error)
+  kill(false)
+})
