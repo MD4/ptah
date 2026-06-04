66dcea852687f16d7d5c88edc6e9ffd4fc416cf0
MD4 <m.dequa@gmail.com>
2026-06-04 02:16:07 +0200

    feat(lib-shared): add .backups path constants

--- packages/lib-shared/src/env/vars.env.ts
+++ packages/lib-shared/src/env/vars.env.ts
@@ -7,3 +7,8 @@ export const PTAH_SETTINGS_FILENAME = ".ptah-settings.json";
 export const PTAH_SETTINGS_PATH = `${PTAH_DIRECTORY}/${PTAH_SETTINGS_FILENAME}`;
 export const PTAH_SHOWS_PATH = `${PTAH_DIRECTORY}/shows`;
 export const PTAH_PROGRAMS_PATH = `${PTAH_DIRECTORY}/programs`;
+
+export const PTAH_BACKUPS_PATH = `${PTAH_DIRECTORY}/.backups`;
+export const PTAH_SHOWS_BACKUPS_PATH = `${PTAH_BACKUPS_PATH}/shows`;
+export const PTAH_PROGRAMS_BACKUPS_PATH = `${PTAH_BACKUPS_PATH}/programs`;
+export const PTAH_SETTINGS_BACKUPS_PATH = `${PTAH_BACKUPS_PATH}/settings`;
